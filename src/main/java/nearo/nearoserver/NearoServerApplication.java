7e9672315df2df515cd3ec1cfc4c3d7dc1784d28
Wondear <vytpals00@gmail.com>
2024-11-08 14:55:13 +0900

    [init] initial project maked

---
+++ src/main/java/nearo/nearoserver/NearoServerApplication.java
@@ -0,0 +1,13 @@
+package nearo.nearoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearoServerApplication.class, args);
+    }
+
+}
