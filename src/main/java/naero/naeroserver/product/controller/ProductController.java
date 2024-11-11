a074610cf691d979d004eb6b86f92ad77e29afad
scho123 <nvmsnach@gmail.com>
2024-11-11 14:47:50 +0900

    feat: 상품 등록
    controller에 상품 등록 메서드 추가(미완)

--- src/main/java/naero/naeroserver/product/controller/ProductController.java
+++ src/main/java/naero/naeroserver/product/controller/ProductController.java
@@ -1,7 +1,35 @@
 package naero.naeroserver.product.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import naero.naeroserver.product.dto.ProductDTO;
+import naero.naeroserver.product.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/api/*")
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+//    @Operation(summary = "상품 등록 요청", description = "해당 상품 등록이 진행됩니다.", tags = { "ProductController" })
+//    @PostMapping(value = "/products")
+//    public ResponseEntity<ResponseDTO> insertProduct(@ModelAttribute ProductDTO productDTO, MultipartFile productImage) {
+//
+//        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "상품 입력 성공",  productService.insertProduct(productDTO, productImage)));
+//    }
 }
