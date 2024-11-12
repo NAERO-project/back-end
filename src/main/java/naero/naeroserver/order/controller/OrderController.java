275753ee69aaf6c0f088831f47862867893f714e
scho123 <nvmsnach@gmail.com>
2024-11-12 10:13:18 +0900

    feat: 상품 결제 메서드만 추가

--- src/main/java/naero/naeroserver/order/controller/OrderController.java
+++ src/main/java/naero/naeroserver/order/controller/OrderController.java
@@ -1,5 +1,10 @@
 package naero.naeroserver.order.controller;
 
+import naero.naeroserver.order.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/*")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 }
