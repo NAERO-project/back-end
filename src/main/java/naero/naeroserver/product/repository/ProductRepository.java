8ae29396b755fc8d09a796e809cffc2c3034145e
scho123 <nvmsnach@gmail.com>
2024-11-12 16:36:20 +0900

    feat: 상품 미리보기 조회 기능 구현

--- src/main/java/naero/naeroserver/product/repository/ProductRepository.java
+++ src/main/java/naero/naeroserver/product/repository/ProductRepository.java
@@ -4,10 +4,23 @@ import naero.naeroserver.entity.product.TblProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<TblProduct, Integer> {
+
+    @Query("SELECT p FROM TblProduct p WHERE p.productCheck = 'Y' ORDER BY p.productId DESC")
+    List<TblProduct> findAllProductWithLimit(Pageable pageable);
+
+    @Query("SELECT p FROM TblProduct p " +
+            "JOIN p.smallCategory sc " +
+            "JOIN sc.mediumCategory mc " +
+            "JOIN mc.largeCategory lc " +
+            "WHERE lc.id = :largeCategoryId AND p.productCheck = 'Y' " +
+            "ORDER BY p.productId DESC")
+    List<TblProduct> findByFoodProductWithLimit(@Param("largeCategoryId") Integer largeCategoryId, Pageable pageable);
 
 //    List<TblProduct> findByProductOrderable(String status);
 
