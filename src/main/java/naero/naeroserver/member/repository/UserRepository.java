641b18f96a092467fc2eccec0404efaa7c1974e3
Wondear <vytpals00@gmail.com>
2024-11-19 13:10:42 +0900

    fix: 엔티티 변경으로인한 레포지토리 오류 수정

--- src/main/java/naero/naeroserver/member/repository/UserRepository.java
+++ src/main/java/naero/naeroserver/member/repository/UserRepository.java
@@ -1,13 +1,9 @@
 package naero.naeroserver.member.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import naero.naeroserver.entity.user.TblUser;
-import naero.naeroserver.member.dto.ManageSearchDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,13 +11,9 @@ import java.util.Map;
 
 @Repository
 public interface UserRepository extends JpaRepository<TblUser, Integer> {
-
-
-    TblUser findById(int Id);
     TblUser findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByUserEmail(String email);
-    public TblUser findByIdAndWithStatus(int Id, String status);
 
     TblUser findByUserEmail(String email);
 //    TblUser updateByUsername(TblUser user);
